5d989286c4ffa7465a9c9e78fbd806232010c6bd
pc <macit2534@gmail.com>
2023-05-23 14:07:25 +0300

    menu tıklama sorunu çözüldü

--- app/src/main/java/com/example/projedenemetasarm/Activty/MainActivity222.java
+++ app/src/main/java/com/example/projedenemetasarm/Activty/MainActivity222.java
@@ -1,15 +1,5 @@
 package com.example.projedenemetasarm.Activty;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBarDrawerToggle;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.SearchView;
-import androidx.appcompat.widget.Toolbar;
-import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.navigation.fragment.NavHostFragment;
-import androidx.navigation.ui.NavigationUI;
-
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
@@ -17,12 +7,22 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
+
 import com.example.projedenemetasarm.R;
 import com.google.android.material.navigation.NavigationView;
 
 
 public class MainActivity222 extends AppCompatActivity implements SearchView.OnQueryTextListener {
-
     private NavigationView navim;
     private DrawerLayout drawer;
     private Toolbar toolbar;
@@ -36,54 +36,41 @@ public class MainActivity222 extends AppCompatActivity implements SearchView.OnQ
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main222);
-        //XML verilerinin çekilmesi
-        navim=findViewById(R.id.navigationView);
-        toolbar=findViewById(R.id.toolbar);
-        drawer=findViewById(R.id.drawer);
-        //Navigation ve Toolbar drawer bağlantısının yapıldığı alan
-        NavHostFragment navHostFragment=
-                (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.hostFragment);
-        NavigationUI.setupWithNavController(navim,navHostFragment.getNavController());
 
-        ActionBarDrawerToggle toggle=
-                new ActionBarDrawerToggle(this,drawer,toolbar,0,0);
+        // XML verilerinin çekilmesi
+        NavigationView navim = findViewById(R.id.navigationView);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        DrawerLayout drawer = findViewById(R.id.drawer);
+
+        // Navigation ve Toolbar drawer bağlantısının yapıldığı alan
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.hostFragment);
+        setSupportActionBar(toolbar); // Toolbar'ı ActionBar olarak ayarla
+        NavController navController = navHostFragment.getNavController();
+        NavigationUI.setupActionBarWithNavController(this, navController, drawer); // ActionBar'ı drawer ile ilişkilendir
+        NavigationUI.setupWithNavController(navim, navController); // NavigationView ile NavController'ı ilişkilendir
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, 0, 0);
         drawer.addDrawerListener(toggle);
-        toggle.setDrawerIndicatorEnabled(true);
-
-// ActionBarDrawerToggle ile ilgili işlemleri yönlendir
-        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (drawer.isDrawerOpen(GravityCompat.START)) {
-                    drawer.closeDrawer(GravityCompat.START);
-                } else {
-                    drawer.openDrawer(GravityCompat.START);
-                }
-            }
-        });
-
-// ActionBarDrawerToggle'ın durumunu senkronize et
         toggle.syncState();
-        setSupportActionBar(toolbar);
 
+        getSupportActionBar().setTitle("TARİFEM"); // ActionBar başlığını ayarla
 
-
-        //tasarlanan xml drawer başlangıç menunun bağlanması
-        View baslik=navim.inflateHeaderView(R.layout.drawer_baslik);
-
-       gelenEmail=getIntent().getStringExtra("email");
-       gelenSifre=getIntent().getStringExtra("sifresi");
-
+        // Tasarlanan XML drawer başlangıç menüsünün bağlanması
+        View baslik = navim.inflateHeaderView(R.layout.drawer_baslik);
+        gelenEmail=getIntent().getStringExtra("email");
+        gelenSifre=getIntent().getStringExtra("sifresi");
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         //toolbara searchview in bağlanması
-        getMenuInflater().inflate(R.menu.toolbar_menu,menu);
-        MenuItem item=menu.findItem(R.id.search);
-        SearchView searchView=(SearchView) item.getActionView();
-        searchView.setOnQueryTextListener(this);
-
+        if (getSupportActionBar() != null) {
+            getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+            MenuItem item = menu.findItem(R.id.search);
+            SearchView searchView = (SearchView) item.getActionView();
+            searchView.setOnQueryTextListener(this);
+        }
         return super.onCreateOptionsMenu(menu);
     }
 
@@ -121,7 +108,6 @@ public class MainActivity222 extends AppCompatActivity implements SearchView.OnQ
             super.onBackPressed();
 
         }
+
     }
-
-
 }
